fe8c7aa6cb04393a67903f803e88c375a8598789
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2022-01-30 18:35:42 +0200

    start implement last task from homework

--- 6.1-advanced-functions-exercise/10. Task Manager/app.js
+++ 6.1-advanced-functions-exercise/10. Task Manager/app.js
@@ -1,3 +1,27 @@
 function solve() {
-    console.log("//TODO")
+    const [addTaskSection, openSection, inProgressSection, completeSection] =
+        Array.from(document.querySelectorAll('.wrapper section'));
+
+    addTaskSection.addEventListener('click', onAdd);
+    openSection.addEventListener('click', onOpen);
+    inProgressSection.addEventListener('click', onInProgress);
+    completeSection.addEventListener('click', onComplete);
+
+    function onAdd(event) {
+        event.preventDefault();
+
+        if (event.target.tagName == 'BUTTON' && event.target.id == 'add') {
+            const taskName = document.getElementById('task');
+            const description = document.getElementById('description');
+            const date = document.getElementById('date');
+
+            console.log(taskName.value, description.value, date.value);
+        }
+    }
+
+    function onOpen(event) {}
+
+    function onInProgress(event) {}
+
+    function onComplete(event) {}
 }
